ef750f9a961756b477134fbb1cfb1a6bbe96d3ae
netl <juuso.metsavuori@gmail.com>
2019-08-03 16:35:11 +0300

    added directory listing, removed likes and other username related stuff

--- gallery.py
+++ gallery.py
@@ -9,10 +9,11 @@ app = Flask(__name__)
 def overview(path):
     #directory to watch
     location = 'gallery'
-
+    
+    p = Path(location+"/"+path)
     #recursively find all files in directory
     files = []
-    for f in Path(location+"/"+path).rglob('**/*.*'):
+    for f in p.rglob('**/*.*'):
       files.append({
          "name" : f.name,
          "location" : str(f.relative_to(location).parents[0]),
@@ -22,8 +23,14 @@ def overview(path):
     #sort found files by age
     files = sorted(files, key = lambda x: x['age'])
 
+    #generate directory listing                                                                           
+    dirs = []
+    for f in (p.glob('./*')):
+        if f.is_dir():
+            dirs.append(str(f).replace(location+"/"+path,"").strip("/"))
+
     #render template with files
-    return(render_template('index.html', files = files[:6*4]))
+    return(render_template('index.html', files = files[:6*4], dirs = dirs, path = path))
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000, True)
